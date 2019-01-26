13db7b712483fdb4e1558b0bcbc3dc5e3bf54fde
MSMunusamy <43171766+MSMunusamy@users.noreply.github.com>
2019-01-26 09:15:56 +0530

    Update ls_header.h

--- ls_header.h
+++ ls_header.h
@@ -1,9 +1,9 @@
 /*************************************/
 /* Author      :Munusamy M           */
 /* Date        :22-01-2019           */
-/* Filename    :receiver_p6.c        */
-/* Description :collect all strings  */
-/*             from p1,p2,p3,p4,p5   */
+/* Filename    :header.h             */
+/* Description :Headers              */
+/*                                   */
 /*************************************/
 
 #ifndef  LS_HEADER_H
